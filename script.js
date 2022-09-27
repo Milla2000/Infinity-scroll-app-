c32e3bb1ee45d81d1dd3d05ffa944757947d4d47
Milla2000 <millajesso2000@gmail.com>
2022-09-27 18:16:27 +0300

    update hard coded

--- script.js
+++ script.js
@@ -15,27 +15,44 @@ function setAttributes(element, attributes) {
   }
 }
 
-// Create Elements For Links & Photos, Add to DOM
+//Create Elements For Links & Photos, Add to DOM
 function displayPhotos() {
   // Run function for each object in photosArray
   photosArray.forEach((photo) => {
-    // Create <a> to link to Unsplash
+
+
+    // Create <a>(anchor element) to link to Unsplash
     const item = document.createElement('a');
-    setAttributes(item, {
-      href: photo.links.html,
-      target: '_blank',
-    });
+    item.setAttribute('href', photo.links.html);
+    item.setAttribute('target', '_blank');
+    
+    //creates a blank anchor element
+    // setAttributes(item, {
+    //   href: photo.links.html,
+    //   target: '_blank',
+    // });
+
+
     // Create <img> for photo
     const img = document.createElement('img');
-    setAttributes(img, {
-      src: photo.urls.regular,
-      alt: photo.alt_description,
-      title: photo.alt_description,
-    });
+    img.setAttribute('src', photo.urls.regular);
+    img.setAttribute('alt', photo.alt_description);
+    img.setAttribute('title', photo.alt_description);
+
+
+    // setAttributes(img, {
+    //   src: photo.urls.regular,
+    //   alt: photo.alt_description,
+    //   title: photo.alt_description,
+    // });
+
+  
     // Put <img> inside <a>, then put both inside imageContainer Element
     item.appendChild(img);
     imageContainer.appendChild(item);
   });
+
+
 }
 
 
